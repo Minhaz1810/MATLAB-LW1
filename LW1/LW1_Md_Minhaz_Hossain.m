eb4566992444b930d3d97baf2297bc20776f1ce0
Minhaz1810 <minhajhossain75@gmail.com>
2026-09-11 15:46:45 +0300

    Add files via upload

---
+++ LW1/LW1_Md_Minhaz_Hossain.m
@@ -0,0 +1,27 @@
+% Name: Md Minhaz
+% Surnamre : Hossain
+% Group: EKfu25/1
+% Date: 11-09-2026
+
+clc;
+clear;
+
+x = 0:1:10;
+y = x.^2;
+
+plot(x,y)
+
+xlabel('X-axis')
+ylabel('Y-axis')
+
+title('Graph of y=x^2')
+
+grid on
+
+a = linspace(0,10,5);
+
+sqrt(25)
+
+sin(pi/2)
+
+mean([10 20 30])
